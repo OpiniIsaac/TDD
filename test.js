3f37cb095fd332d5319892558f4d0c3b056767f7
Opini Isaac <isaacopini8@gmail.com>
2023-06-26 11:46:47 +0300

    git ignore file

--- test.js
+++ test.js
@@ -2,7 +2,20 @@ const { assert } = require('chai');
 const multiply = require('./mutiply');
 
 describe('Multiply', () => {
-  it('should multiply 1 and 1', () => {
-    assert.equal(multiply(1, 1), 1);
+  it('first cylces test', () => {
+    assert.equal(multiply(2, 2), 4);
   });
+  it('second cylces test', () => {
+    assert.equal(multiply(2, 2), 4);
+  });
+  it('third cylces test', () => {
+    assert.equal(multiply(3, 3), 9);
+  });
+  it('fourth  cylces test', () => {
+    assert.equal(multiply(4, 4), 16);
+  });
+  it('third cylces test', () => {
+    assert.equal(multiply(23, 45), 23 * 45 );
+  });
+
 });
